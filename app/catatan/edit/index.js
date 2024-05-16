70043bf652ea46ab95e5abb912cd5ca7c92e5bf0
TheValeHack <email.penting.janngan.kepo@gmail.com>
2024-05-16 23:12:45 +0700

    feat[writing]: Add writing by voice feature

--- app/catatan/edit/index.js
+++ app/catatan/edit/index.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import Layout from '../../../src/layout/layout'
 import CurrentPage from '../../../src/components/kelas/CurrentPage'
-import { RichEditor } from 'react-native-pell-rich-editor'
 import { View } from 'react-native'
+import CatatanEditor from '../../../src/components/catatan/CatatanEditor'
 
 export default function App() {
   return (
     <Layout>
         <CurrentPage page={"Catatan"} />
-        <View>
-        <RichEditor
-        initialContentHTML={'Hello <b>World</b> <p>this is a new paragraph</p> <p>this is another new paragraph</p>'}
-        />
+          <View>
+            <CatatanEditor />
         </View>
     </Layout>
   )
