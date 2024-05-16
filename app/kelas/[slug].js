d732c1dd5e8763a0f7c8ce9e3b937d3dfba4d26f
gnafhan <nafhanghifari@gmail.com>
2024-05-16 22:40:07 +0700

    feat[data]: add format for fetching data

---
+++ app/kelas/[slug].js
@@ -0,0 +1,44 @@
+import { Image, ScrollView, Text, View } from 'react-native'
+import CurrentPage from '../../src/components/kelas/CurrentPage'
+import ScrollNumber from '../../src/components/quiz/ScrollNumber'
+import Layout from '../../src/layout/layout'
+import biologi from '../../assets/biologi.png'
+import {
+  Collapse,
+  CollapseHeader,
+  CollapseBody,
+  AccordionList
+} from 'accordion-collapse-react-native'
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5'
+import { Accordion } from '../../src/components/kelas/Accordion'
+import { useLocalSearchParams } from 'expo-router'
+
+export default function App () {
+    const {slug} = useLocalSearchParams()
+    //TODO: fetch data from API
+  return (
+    <Layout>
+      <CurrentPage page={'Kelas'} />
+      <View className='flex flex-row items-center justify-between w-full px-[7vw] '>
+        <View>
+          <Text className='text-2xl font-bold'>Biologi</Text>
+          <Text className='mt-2 text-lg text-secondary'>
+            Sri Windarti, S.Pd
+          </Text>
+        </View>
+        <View>
+          <Image className='w-36 h-36' source={biologi} alt='aa' />
+        </View>
+      </View>
+      <View className='relative min-h-[52vh] max-h-[52vh]'>
+        <View className='bg-[#7A87C4] rounded-t-[30px] opacity-20 w-full min-h-[50vh] h-full absolute top-0'></View>
+        <ScrollView>
+          <View className='flex gap-3 mx-5 mt-7'>
+            <Accordion title='Pertemuan 1: Gagak ' />
+            <Accordion title='Pertemuan 2: ' />
+          </View>
+        </ScrollView>
+      </View>
+    </Layout>
+  )
+}
--- src/components/home/MatkulCard.js
+++ src/components/home/MatkulCard.js
@@ -3,28 +3,28 @@ import {
   View,
   Text,
   ScrollView,
-  Image,
   StyleSheet
 } from 'react-native'
 import biologi from '../../../assets/biologi.png'
 import { router } from 'expo-router'
+import { Image } from 'expo-image'
 
 
-function MatkulCard () {
+function MatkulCard ({judul, pengajar, image, id_kelas}) {
   return (
-    <View onTouchStart={()=>router.push('/kelas')} style={{shadowOpacity: 3, shadowColor: '#7F7F82', shadowOffset: 10, shadowRadius: 10}} className='w-[48%] aspect-square h-auto justify-between py-2 px-3 bg-white shadow-md  mt-4 rounded-[17px] '>
+    <View onTouchStart={()=>router.push(`/kelas/${id_kelas}`)} style={{shadowOpacity: 3, shadowColor: '#7F7F82', shadowOffset: 10, shadowRadius: 10}} className='w-[48%] aspect-square h-auto justify-between py-2 px-3 bg-white shadow-md  mt-4 rounded-[17px] '>
       <View className='flex items-end justify-end w-full '>
-        <Image className='w-28 h-28' source={biologi} alt='aa' />
+        <Image className='w-28 h-28' source={image} alt='aa' />
       </View>
       <View>
         <Text style={styles.medium} className='text-xl'>
-          Biologi
+          {judul}
         </Text>
         <Text
           style={styles.regular}
           className='text-normal text-[#7F7F82] line-clamp-1'
         >
-          Susi Susanti, S.Pd
+          {pengajar}
         </Text>
       </View>
     </View>
